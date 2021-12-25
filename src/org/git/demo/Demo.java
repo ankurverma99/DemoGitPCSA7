9ed1497f17998748a576025848644b2e23084193
ankurverma99 <59686221+ankurverma99@users.noreply.github.com>
2021-12-25 11:29:59 +0530

    Update Demo.java
    kgkgkgkgkkgjvjvjvjvjujvjvjvjvjvjgvjk

--- src/org/git/demo/Demo.java
+++ src/org/git/demo/Demo.java
@@ -3,6 +3,9 @@ package org.git.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Merry X-mas");
+		//fix the mistek//
+		int i=25;
+		System.out.println(i);
 	}
 
 }
